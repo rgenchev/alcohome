17a22396d234af71084d9625ad1afb08fefcae92
genchevv <r.genchew@gmail.com>
2018-04-10 00:01:28 +0200

    add category reference to Product model

---
+++ db/migrate/20180409214334_add_category_reference_to_products.rb
@@ -0,0 +1,5 @@
+class AddCategoryReferenceToProducts < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :products, :category, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409212709) do
+ActiveRecord::Schema.define(version: 20180409214334) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180409212709) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
