17469b24e44834ee6dd9ce2e7bb4742b388c7900
genchevv <r.genchew@gmail.com>
2018-04-11 01:27:17 +0200

    add custom validation method for limiting creation of categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,13 @@
 class Category < ApplicationRecord
 	has_many :products
+
+  validate :limited_categories
+
+  private
+
+    def limited_categories
+      if Category.count == 9
+        errors[:base] << "There is a limited number of categories."
+      end
+    end
 end
