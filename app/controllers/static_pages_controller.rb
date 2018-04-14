fd86791c70a35003879fb007baa54f5f1b2057f5
genchevv <r.genchew@gmail.com>
2018-04-15 00:42:05 +0200

    create category instance variables for the homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
 	def home
+    @category_schnaps_and_minis = Category.find_by(name: "Schnaps & Minis")
+    @category_gin = Category.find_by(name: "Gin")
+    @category_rum = Category.find_by(name: "Rum")
+    @category_vodka = Category.find_by(name: "Vodka")
+    @category_whiskey = Category.find_by(name: "Whiskey")
+    @category_wine = Category.find_by(name: "Wein")
 	end
 end
