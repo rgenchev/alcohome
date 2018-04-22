3c39f386cea2f6c80e3fa1c374e3af4544b8f23c
genchevv <r.genchew@gmail.com>
2018-04-22 22:57:19 +0200

    add 'role' attr to User model and enumerize it

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -10,6 +10,11 @@ class UserDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     email: Field::String,
+    password: Field::String,
+    password_confirmation: Field::String,
+    role: Field::Select.with_options(
+      collection: User.roles.keys
+    ),
     encrypted_password: Field::String,
     reset_password_token: Field::String,
     reset_password_sent_at: Field::DateTime,
@@ -31,8 +36,7 @@ class UserDashboard < Administrate::BaseDashboard
   COLLECTION_ATTRIBUTES = [
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
+    :role
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -40,17 +44,7 @@ class UserDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = [
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
-    :created_at,
-    :updated_at,
+    :role
   ].freeze
 
   # FORM_ATTRIBUTES
@@ -58,15 +52,9 @@ class UserDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
+    :password,
+    :password_confirmation,
+    :role
   ].freeze
 
   # Overwrite this method to customize how users are displayed
