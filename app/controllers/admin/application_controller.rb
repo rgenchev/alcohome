82051cfd037dc65e3fc8d48febeb7de7342450ea
genchevv <r.genchew@gmail.com>
2018-04-22 23:02:57 +0200

    remove 'authenticate_user' callback from admin app controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,7 +6,6 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_user!
     before_action :authenticate_admin
 
     def authenticate_admin
