b16b198f09b74ad951aef356de4bf8b726fcdddc
genchevv <r.genchew@gmail.com>
2018-04-27 18:17:17 +0200

    integrate Paperclip in Product model and categories show view page

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,9 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :vendor
 
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
   validates :name, presence: true
   validates :price, presence: true
   validates :category, presence: true
