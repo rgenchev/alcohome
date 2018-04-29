55d27dea914b73d668fb62c70dee0fac5c986aa7
yge <Yosif.Genchev@softwareag.com>
2018-04-29 18:57:47 +0300

    Category randomness

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,10 @@
 class StaticPagesController < ApplicationController
 	def home
-    @category_schnaps_and_minis = Category.find_by(name: "Schnaps & Minis")
-    @category_gin = Category.find_by(name: "Gin")
-    @category_rum = Category.find_by(name: "Rum")
-    @category_vodka = Category.find_by(name: "Vodka")
-    @category_whiskey = Category.find_by(name: "Whiskey")
-    @category_wine = Category.find_by(name: "Wine")
+    @arr = Category.all.to_a.sample 3
+
+    @category_random = @arr[0]
+    @category_random2 = @arr[1]
+    @category_random3 = @arr[2]
 	end
 
   def about
