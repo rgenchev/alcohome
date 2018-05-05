082d53a50d4b553e4eb19b034c3c8a05ef4d1854
Radoslav Genchev <r.genchew@gmail.com>
2018-05-05 16:28:26 +0200

    set limit 3 for 'products on sale' on homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
     @category_random2 = @arr[1]
     @category_random3 = @arr[2]
 
-    @on_sale_products = Product.order("RANDOM()").on_sale.limit(4)
+    @on_sale_products = Product.order("RANDOM()").on_sale.limit(3)
 	end
 
   def about
