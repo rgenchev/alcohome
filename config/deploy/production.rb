13c6027023ca8373fbb3e10d88d08227a34743be
Radoslav Genchev <r.genchew@gmail.com>
2018-05-19 23:41:44 +0200

    connect and become 'root'

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, %w{alcohome@alcohome}
-role :web, %w{alcohome@alcohome}
-role :db,  %w{alcohome@alcohome}
+role :app, %w{root@alcohome}
+role :web, %w{root@alcohome}
+role :db,  %w{root@alcohome}
 
 # server-based syntax
 # ======================
