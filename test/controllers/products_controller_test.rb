7e0d3dcdf95deb26461f6183e1eec7234304a654
Radoslav Genchev <r.genchew@gmail.com>
2018-05-21 01:47:54 +0200

    remove test related to 'product' index page

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -7,11 +7,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = Product.create(name: "Grey Goose", price: 35.00, category: category, vendor: vendor, image: fixture_file_upload('images/test_product_image.jpg'))
   end
 
-  test "should get index" do
-    get products_url
-    assert_response :success
-  end
-
   test "should show product" do
     get product_url(@product)
     assert_response :success
