2d718eb63da9b1412074d4b5ed6205e098df1d81
Radoslav Genchev <r.genchew@gmail.com>
2018-05-21 20:30:37 +0200

    add image attr to the newly created 'category' record

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    category = Category.create(name: "Vodka")
-    vendor = Vendor.create(name: "Grey Goose")
-    @product = Product.create(name: "Grey Goose", price: 35.00, category: category, vendor: vendor, image: fixture_file_upload('images/test_product_image.jpg'))
+    category = Category.create!(name: "Vodka", image: fixture_file_upload('images/test_category_image.jpg'))
+    vendor = Vendor.create!(name: "Grey Goose")
+    @product = Product.create!(name: "Grey Goose", price: 35.00, category: category, vendor: vendor, image: fixture_file_upload('images/test_product_image.jpg'))
   end
 
   test "should show product" do
