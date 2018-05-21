e75d5d81eac53bdda37d5bf7f8adc13ed8fb82df
Radoslav Genchev <r.genchew@gmail.com>
2018-05-21 20:31:39 +0200

    add controller tests for 'home', 'about' and 'contact'

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    category_1 = Category.create!(name: "Vodka", image: fixture_file_upload('images/test_category_image.jpg'))
+    category_2 = Category.create!(name: "Gin", image: fixture_file_upload('images/test_category_image.jpg'))
+    category_3 = Category.create!(name: "Whiskey", image: fixture_file_upload('images/test_category_image.jpg'))
+  end
+
+  test "should get home" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+  end
 end
