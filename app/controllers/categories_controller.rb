a26b0cfab73511c9f7b36465cc291945262c35e7
Radoslav Genchev <r.genchew@gmail.com>
2018-05-27 15:46:22 +0200

    bring back 'Soft' category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.where.not(name: ["Soft", "Spirits", "Minis"])
+    @categories = Category.where.not(name: ["Spirits", "Minis"])
 
     authorize @categories
   end
