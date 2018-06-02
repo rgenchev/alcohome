d5c12d15c1008869618f4f60c510c2a5fb90c276
Radoslav Genchev <r.genchew@gmail.com>
2018-06-02 17:14:04 +0200

    add 'Cognac' category to the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
   def create_categories
-    category_names = ["Champagne", "Wine", "Vodka", "Gin", "Whiskey", "Rum", "Minis", "Soft", "Snacks", "Beer", "Liquor", "Tequila", "Absinthe"]
+    category_names = ["Champagne", "Cognac", "Wine", "Vodka", "Gin", "Whiskey", "Rum", "Minis", "Soft", "Snacks", "Beer", "Liquor", "Tequila", "Absinthe"]
 
     category_names.each do |name|
       Category.create!(name: name, image: fixture_file_upload('images/test_category_image.jpg'))
